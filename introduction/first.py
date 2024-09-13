a09cc95d7215da11902238f0807d98cad02759ea
shourav kumar <shouravk81@gmail.com>
2024-09-13 12:35:37 +0530

    work

---
+++ introduction/first.py
@@ -0,0 +1,5 @@
+print("shourav kumar")
+print("is best guy")
+print("shourav kumar","is best guy")
+print(12)
+print(33-33)
---
+++ introduction/second.py
@@ -0,0 +1,8 @@
+name= "shourav kumar"
+age = 21
+price = 33.69
+age2 = age
+
+print(name)
+print(age2)
+print(price)
